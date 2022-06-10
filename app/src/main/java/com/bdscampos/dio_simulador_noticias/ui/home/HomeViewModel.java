6fc9f25e9e8b8ecaf6af12d062ec081caa03753a
Bruno Campos <bdscampos@gmail.com>
2022-06-09 21:00:38 -0300

    Configuração inicial do projeto

---
+++ app/src/main/java/com/bdscampos/dio_simulador_noticias/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.bdscampos.dio_simulador_noticias.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
